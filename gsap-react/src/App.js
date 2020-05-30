a7745d24a5f267c38049d819087c0f30e339aa0e
joker7t <batoan131196@gmail.com>
2020-05-31 00:21:13 +0700

    add natours project

--- gsap-react/src/App.js
+++ gsap-react/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.scss';
 import MainPage from './components/MainPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NatoursPage from './components-natours/NatoursPage';
+
 
 const App = () => {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/alpha' component={MainPage} />
+          <Route exact path='/natours' component={NatoursPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
